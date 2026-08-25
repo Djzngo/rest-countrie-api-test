60bedaf59d50ffa925fdb4ccb8b9ab14c98c484e
Djzngo <nimi_9@hotmail.co.uk>
2026-08-25 08:23:21 +0100

    added countryNames function to script for pulling country name from API GET

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.getElementById("btnRequest").addEventListener("click", e => getRequest(e));
 document.getElementById("btnRequest2").addEventListener("click", e => displayData());
 
+//Request for information from API
 async function getRequest(e) {
     const response = await fetch(
-      "https://api.restcountries.com/countries/v5",
+      "https://api.restcountries.com/countries/v5?region=Europe",
       {
         headers: {
           Authorization: "Bearer rc_live_79b6961d3d694fd19986cb45e54a3983",
@@ -15,6 +16,7 @@ async function getRequest(e) {
 
     if (response.ok) {
         countryNames(data);
+        console.log(data);
     }
     else {
         throw new Error(data.error);
@@ -25,6 +27,11 @@ function countryNames(data) {
     const test = data;
     let testArray = [];
 
+    for (let i = 0; i <= 10; i++) {
+        testArray.push(test.data.objects[i].names.common);
+        console.log(testArray)
+    }
+
     // Go through object and pulls common name for country out of the object.
-    console.log(test.data.objects[1].names.common);
+    console.log(test.data.objects[2].names.common);
 }
